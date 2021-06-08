f1fa5806e947bd4cf98179482052c54e2528798f
Nikki Mallory <nikkimallory13@gmail.com>
2021-06-08 07:51:21 -0500

    server connected, new filess add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-// const Pokemon = require('../models/pokemon.js');
+const Pokemon = require('./models/pokemon.js');
 
 
 // // INDEX
@@ -13,6 +13,29 @@ const PORT = 3000;
 // // SHOW
 // app.get('/:id', (req, res) => {
 // res.render('show.ejs', { data: Pokemon[req.params.id] });
+// });
+
+// // New
+// app.get('/pokemon/new', (req, res) => {
+//     res.send('new.ejs', {data: Pokemon[req.params.id ]});
+// });
+
+// // Edit
+// app.get('/pokemon/:id/edit', (req, res) => {
+    
+// });
+
+// // Create
+// app.post('pokemon', (req, res) =>{
+
+// });
+// // Update
+// app.put('/pokemon/:id', (req, res) => {
+
+// });
+// // Delete
+// app.delete('/pokemon/:id', (req, res) => {
+
 // });
 
 // Listener/Server
